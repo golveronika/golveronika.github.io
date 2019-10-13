d0c4be4a75ea8ba293997327fa38bc19673a7c9c
Veronika Golovanova <vgolovanova@actum.cz>
2019-10-13 17:07:36 +0200

    Jiri task - The Classroom -  Demo

---
+++ TheClassroom/precache-manifest.456b1951f28232faa92ee69c938a501d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36b2289abb947b60cc1bc943d387c614",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c89b10092b46b2963dd9",
+    "url": "/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "c89b10092b46b2963dd9",
+    "url": "/static/js/2.d5acf49a.chunk.js"
+  },
+  {
+    "revision": "fa52490cc1cfa73fb6c8",
+    "url": "/static/js/main.964488d7.chunk.js"
+  },
+  {
+    "revision": "0c1400ec95fcc8fa04a7",
+    "url": "/static/js/runtime~main.5134c318.js"
+  }
+]);
